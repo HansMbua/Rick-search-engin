378f308cc453d1824a74ce287591f0e7de07624b
hansmbua <hansmbua@yahoo.com>
2021-11-27 08:37:30 +0100

    added: Inventory, addGuitar class

--- src/com/company/Inventory.java
+++ src/com/company/Inventory.java
@@ -1,6 +1,21 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Inventory {
+    private Guitar guitar;
+    private List<Guitar> GuiterList;
 
+    public Inventory() {
 
+        GuiterList = new ArrayList<>();
+    }
+    //addeds guitar object to Inventory
+    public void addGuitar(String serialNumber, String builder, String model, String type, String backwood, String topwood, double price){
+        //get guiter
+     guitar = new Guitar(serialNumber,builder,model,type,backwood,topwood,price);
+        //add guiter to inventory
+        GuiterList.add(guitar);
+    }
 }
