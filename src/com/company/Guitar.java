a714a0ce521220e473badf7a4ec1bbfbd5ddc16d
Hans Mbua <hansmbua@yahoo.com>
2021-11-27 08:06:32 +0100

    new commit

---
+++ src/com/company/Guitar.java
@@ -0,0 +1,62 @@
+package com.company;
+
+public class Guitar {
+    private String serialNumber,builder,model,type,backwood,topwood;
+    private double price;
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getBuilder() {
+        return builder;
+    }
+
+    public void setBuilder(String builder) {
+        this.builder = builder;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBackwood() {
+        return backwood;
+    }
+
+    public void setBackwood(String backwood) {
+        this.backwood = backwood;
+    }
+
+    public String getTopwood() {
+        return topwood;
+    }
+
+    public void setTopwood(String topwood) {
+        this.topwood = topwood;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
